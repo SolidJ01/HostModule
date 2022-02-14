1b163f00d64acbf817ace0d1e5fb640a69f43dfd
SolidJ01 <josef.f.fast@gmail.com>
2022-02-14 14:46:23 +0100

    Began transition to API

---
+++ HostAPI/Models/HostContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HostAPI.Models;
+
+namespace HostAPI.Models
+{
+    public class HostContext : DbContext
+    {
+        DbSet<Host> Hosts;
+    }
+}
