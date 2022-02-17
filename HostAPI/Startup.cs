e9c64e9097d794cfe3b0f2062ec9a00c9f62da93
Josef Fast <josef.f.fast@gmail.com>
2022-02-17 16:28:02 +0100

    Minor tweak

--- HostAPI/Startup.cs
+++ HostAPI/Startup.cs
@@ -45,7 +45,7 @@ namespace HostAPI
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HostAPI v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("v1/swagger.json", "HostAPI v1"));
             }
 
             app.UseRouting();
