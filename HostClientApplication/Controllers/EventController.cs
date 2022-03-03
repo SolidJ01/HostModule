a23be2e099eb67fa5ff3e5ad4ce31ef78652a895
SolidJ01 <josef.f.fast@gmail.com>
2022-03-03 16:02:25 +0100

    Integrated Event Creation Function

---
+++ HostClientApplication/Controllers/EventController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HostClientApplication.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        public IActionResult Detalis()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ HostClientApplication/Models/Event.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HostClientApplication.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Location { get; set; }
+        public string City { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string Category { get; set; }
+        public string Priority { get; set; }
+        public string Image { get; set; }
+        public int VenueId { get; set; }
+    }
+}
