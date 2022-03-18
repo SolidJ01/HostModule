3b63cb377ed7d073423e9b7f52fe5357db53b5f5
Josef Fast <josef.f.fast@gmail.com>
2022-03-18 12:36:06 +0100

    Eventlist, Productlist implemented

--- HostClientApplication/Controllers/EventController.cs
+++ HostClientApplication/Controllers/EventController.cs
@@ -2,15 +2,78 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using HostClientApplication.Models;
+using Newtonsoft.Json;
+using System.Security.Claims;
 
 namespace HostClientApplication.Controllers
 {
     public class EventController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            HttpClient cl = new HttpClient();
+            List<Event> events = new List<Event>();
+            var response = await cl.GetAsync("http://193.10.202.74/EventAPI1/api/Events");
+            string jsonresponse = await response.Content.ReadAsStringAsync();
+            events = JsonConvert.DeserializeObject<List<Event>>(jsonresponse).ToList();
+
+            List<Event> goodevents = new List<Event>();
+            Random rand = new Random();
+            foreach (Event e in events)
+            {
+                if (e.HostId == int.Parse (User.FindFirstValue(ClaimTypes.NameIdentifier)))
+                {
+                    if (e.Booking == null)
+                    {
+                        e.Booking = new List<Booking>();
+                        int roof = rand.Next(e.TicketsMax / 3, e.TicketsMax + 1);
+                        for (int i = 0; i < roof; i++)
+                        {
+                            Booking booking = new Booking();
+                            e.Booking.Add(booking);
+                        }
+                    }
+                    e.BookedPercentage = (decimal.Divide(e.Booking.Count, e.TicketsMax) * 100).ToString().Replace(",", ".");
+                    goodevents.Add(e);
+                }
+            }
+
+
+
+            return View(goodevents);
+        }
+
+        public async Task<IActionResult> Products(int id)
+        {
+            HttpClient cl = new HttpClient();
+            EventProducts eventProducts = new EventProducts();
+            using (var response = await cl.GetAsync("http://193.10.202.74/EventAPI1/api/Events/" + id))
+            {
+                string jsonresponse = await response.Content.ReadAsStringAsync();
+                Event ev = JsonConvert.DeserializeObject<Event>(jsonresponse);
+                eventProducts.Id = ev.Id;
+                eventProducts.Name = ev.Name;
+            }
+
+            using (HttpResponseMessage response = await cl.GetAsync("http://193.10.202.71/GroupOne_WebApi/api/Products/Event/" + id))
+            {
+                string jsonresponse = await response.Content.ReadAsStringAsync();
+                try
+                {
+                    List<Product> products = JsonConvert.DeserializeObject<List<Product>>(jsonresponse);
+                    eventProducts.Products = products;
+                }
+                catch
+                {
+                    Console.WriteLine("Event has no products");
+                    eventProducts.Products = new List<Product>();
+                }
+            }
+
+            return View(eventProducts);
         }
 
         public IActionResult Create()
--- HostClientApplication/Models/Event.cs
+++ HostClientApplication/Models/Event.cs
@@ -9,16 +9,23 @@ namespace HostClientApplication.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public DateTime Date { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
+        public string Date { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
         public string Location { get; set; }
         public string City { get; set; }
         public string Description { get; set; }
-        public int Price { get; set; }
+        public float Price { get; set; }
         public string Category { get; set; }
         public string Priority { get; set; }
-        public string Image { get; set; }
+        public int TicketsMax { get; set; }
+        public int LocaleId { get; set; }
+        public int HostId { get; set; }
+        public string Picture { get; set; }
         public int VenueId { get; set; }
+        public List<Booking> Booking { get; set; }
+
+        // "Local" attributes
+        public string BookedPercentage { get; set; }
     }
 }
