942b53c3076ba7859047dedab2ff20915f6f07a0
Florimmehmeti <Florimmehmeti7@gmail.com>
2022-03-18 14:24:58 +0100

    Adminpanel för arrangörsredigering färdig

---
+++ AdminClientApplication/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function SubmitProfile() {
+    description = document.getElementById("description");
+    descriptionhidden = document.getElementById("descriptionhidden");
+
+    descriptionhidden.value = new DOMParser().parseFromString(description.innerHTML, 'text/html').body.textContent;
+
+    form = document.getElementById("profileform");
+    form.submit();
+}
